b484a8a13e6cd164ffc4a7335260a87871ef46a0
du <duyux@163.com>
2025-07-03 15:48:43 +0800

    20250703

---
+++ learn-ts-vue202507/HelloWorld.ts
@@ -0,0 +1,29 @@
+function getString(str:String):String{
+    return "Hello,"+str;
+}
+let str:String="TypeScript!!!"
+console.log(getString(str));
+
+// var list1:Array<number>=[1,2,3];
+// var list2:number[]=[1,2,3];
+
+// enum Result{
+//     Success=1, //表示成功
+//     Fail=2     //表示失败
+// }
+
+// enum Direction{
+//     Up="Up",
+//     Down="Down",
+//     Left="Left",
+//     Right="Right"
+// }
+// var direction:Direction=Direction.Up;
+// console.log(direction);
+
+
+//先赋值为数字
+var some1:any=1;
+//后修改为字符串
+some1="Hello any type"
+console.log(some1)
